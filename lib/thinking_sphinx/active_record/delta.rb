a23388199b54c5ed6d4e6cf048f93d8660f1c74e
Nick Muerdter <spam@nickm.org>
2008-05-31 18:15:17 -0400

    Use after_commit plugin I made (http://github.com/GUI/after_commit) to properly handle nested transactions for delta updates.

--- lib/thinking_sphinx/active_record/delta.rb
+++ lib/thinking_sphinx/active_record/delta.rb
@@ -5,63 +5,8 @@ module ThinkingSphinx
     # index_delta, but not sure what reason why.
     # 
     module Delta
-      # Code for after_commit callback is written by Eli Miller:
-      # http://elimiller.blogspot.com/2007/06/proper-cache-expiry-with-aftercommit.html
-      # with slight modification from Joost Hietbrink.
-      #
       def self.included(base)
         base.class_eval do
-          # The define_callbacks method was added post Rails 2.0.2 - if it
-          # doesn't exist, we define the callback manually
-          #
-          if respond_to?(:define_callbacks)
-            define_callbacks :after_commit
-          else
-            class << self
-              # Handle after_commit callbacks - call all the registered callbacks.
-              #
-              def after_commit(*callbacks, &block)
-                callbacks << block if block_given?
-                write_inheritable_array(:after_commit, callbacks)
-              end
-            end
-          end
-          
-          def after_commit
-            # Deliberately blank.
-          end
-          
-          # Normal boolean save wrapped in a handler for the after_commit
-          # callback.
-          # 
-          def save_with_after_commit_callback(*args)
-            value = save_without_after_commit_callback(*args)
-            callback(:after_commit) if value
-            return value
-          end
-          
-          alias_method_chain :save, :after_commit_callback
-          
-          # Forceful save wrapped in a handler for the after_commit callback.
-          #
-          def save_with_after_commit_callback!(*args)
-            value = save_without_after_commit_callback!(*args)
-            callback(:after_commit) if value
-            return value
-          end
-          
-          alias_method_chain :save!, :after_commit_callback
-          
-          # Normal destroy wrapped in a handler for the after_commit callback.
-          #
-          def destroy_with_after_commit_callback
-            value = destroy_without_after_commit_callback
-            callback(:after_commit) if value
-            return value
-          end
-          
-          alias_method_chain :destroy, :after_commit_callback
-          
           private
           
           # Set the delta value for the model to be true.
